bee3f9fe100c489392c6a5948253efd598d0a17b
hanily <rhanilyrondon1115015@gmail.com>
2023-11-30 08:58:07 -0400

    Atividade Incompleta

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,29 @@
+import '../models/user.dart';
+
+Map<String, User> DUMMY_USERS = {
+'1': User(
+  id:'1', 
+  nome:'celeste', 
+  email:'celeste@gmail.com', 
+  avatarURL:'https://cdn.pixabay.com/photo/2013/07/13/10/52/fawkes-157941_1280.png',
+  ),
+'2': User(
+  id:'1', 
+  nome:'celeste', 
+  email:'celeste@gmail.com', 
+  avatarURL:'https://cdn.pixabay.com/photo/2018/04/07/20/37/man-3299537_1280.png',
+  ),
+
+  '3': User(
+  id:'1', 
+  nome:'celeste', 
+  email:'celeste@gmail.com', 
+  avatarURL:'https://cdn.pixabay.com/photo/2015/11/11/12/27/tutankhamun-1038544_1280.jpg',
+  ),
+
+  '4': User(
+  id:'1', 
+  nome:'celeste', 
+  email:'celeste@gmail.com', 
+  avatarURL:''),
+};
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import './data/dummy_users.dart';
+
+void main() {
+  runApp(const MinhaAplicacao());
+}
+
+void runApp(MinhaAplicacao minhaAplicacao) {
+
+}
+class MinhaAplicacao extends StatelessWidget {
+  const MinhaAplicacao({super.key}) {
+    
+  }
+
+  @override
+  Placeholder build(BuildContext context){
+    return const Placeholder();
+  }
+
+}
+
+class Widget {
+}
+
+class Placeholder {
+  const Placeholder();
+}
+
+class BuildContext {
+}
+
+class StatelessWidget {
+}
